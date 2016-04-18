3f11d8183fa989ba5ddae701bfc2f34106185625
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-04-18 16:57:01 +0200

    Switched to an OO implementation

--- poker.py
+++ poker.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python2
-import sys
-k=5
-d=10
-classes = [0 for _ in range(k)]
-while True:
-	flags = [False for _ in range(d)]
-	count = 0 #different values = class
-	stop = False #to leave the outer loop
-	for _ in range(k):
-		digit = sys.stdin.read(1)
-		if digit == "":
-			stop = True
-			break
-		elif digit.isdigit():
-			digit = int(digit)
+
+class Poker(object):
+	def process(self, digits):
+		flags = [False for _ in range(self.d)]
+		count = 0 #different values = class
+		assert(len(digits) == self.k)
+		for digit in digits:
 			if not flags[digit]:
 				flags[digit] = True
 				count += 1
-	if stop:
-		break
-	else:
-		classes[count-1] += 1
-print classes
+		self.classes[count-1] += 1
+	def __init__(self, k):
+		self.k = k
+		self.d = 10
+		self.classes = [0 for _ in range(self.d)]
+
+def read(stream, k, wait_point=True):
+	poker = Poker(k)
+	while True:
+		l = []
+		while len(l)<k:
+			c = stream.read(1)
+			if c == "":
+				return poker.classes
+			elif c == ".":
+				wait_point = False
+			elif c.isdigit() and not wait_point:
+				l.append(int(c))
+		poker.process(l)
+	return poker.classes
+
+import sys
+k=5
+print read(sys.stdin, k)
