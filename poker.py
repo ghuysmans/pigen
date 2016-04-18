e8cb500bf47a081904ad8c649ad16adfbecf9007
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-04-18 16:31:41 +0200

    Implemented a simple poker test

--- poker.py
+++ poker.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python2
 import sys
-print "."
+k=5
+d=10
+classes = [0 for _ in range(k)]
 while True:
-	d = sys.stdin.read(1)
-	if d == "":
+	flags = [False for _ in range(d)]
+	count = 0 #different values = class
+	stop = False #to leave the outer loop
+	for _ in range(k):
+		digit = sys.stdin.read(1)
+		if digit == "":
+			stop = True
+			break
+		elif digit.isdigit():
+			digit = int(digit)
+			if not flags[digit]:
+				flags[digit] = True
+				count += 1
+	if stop:
 		break
-	elif d.isdigit():
-		d = int(float(d)/10*6)
-		sys.stdout.write(str(d))
-print
+	else:
+		classes[count-1] += 1
+print classes
